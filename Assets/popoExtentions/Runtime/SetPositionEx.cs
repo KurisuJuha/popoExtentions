286f3b86fb367d8a75afd03a4d61b6539fed5075
KYapp <kukiyomenaisapp@gmail.com>
2023-03-05 19:11:00 +0900

    SetPositionXを作成

--- Assets/popoExtentions/Runtime/SetPositionEx.cs
+++ Assets/popoExtentions/Runtime/SetPositionEx.cs
@@ -4,5 +4,10 @@ namespace JuhaKurisu.PopoTools.Extentions
 {
     public static class SetPositionEx
     {
+        public static Transform SetPositionX(this Transform transform, float x)
+        {
+            transform.position = new(x, transform.position.y, transform.position.z);
+            return transform;
+        }
     }
 }
