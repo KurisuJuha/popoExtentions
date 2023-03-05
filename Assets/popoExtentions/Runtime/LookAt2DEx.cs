08afdea179e8437191d73bd095172a5110cb6234
KYapp <kukiyomenaisapp@gmail.com>
2023-03-05 19:04:58 +0900

    vector2のlookat2d

--- Assets/popoExtentions/Runtime/LookAt2DEx.cs
+++ Assets/popoExtentions/Runtime/LookAt2DEx.cs
@@ -4,6 +4,7 @@ namespace JuhaKurisu.PopoTools.Extentions
 {
     public static class LookAt2DEx
     {
+        public static void LookAt2D(this Transform transform, Vector2 targetPosition) => transform.LookAt2D(targetPosition);
         public static void LookAt2D(this Transform transform, Vector3 targetPosition)
         {
             Vector3 diff = (targetPosition - transform.position);
