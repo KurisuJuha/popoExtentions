ca21ed1ca919b3769b19b98083824e75210cc98b
KYapp <kukiyomenaisapp@gmail.com>
2023-03-05 19:10:39 +0900

    不要な括弧を削除

--- Assets/popoExtentions/Runtime/LookAt2DEx.cs
+++ Assets/popoExtentions/Runtime/LookAt2DEx.cs
@@ -7,7 +7,7 @@ namespace JuhaKurisu.PopoTools.Extentions
         public static void LookAt2D(this Transform transform, Vector2 targetPosition) => transform.LookAt2D(targetPosition);
         public static void LookAt2D(this Transform transform, Vector3 targetPosition)
         {
-            Vector3 diff = (targetPosition - transform.position);
+            Vector3 diff = targetPosition - transform.position;
             transform.rotation = Quaternion.FromToRotation(Vector3.up, diff);
         }
     }
