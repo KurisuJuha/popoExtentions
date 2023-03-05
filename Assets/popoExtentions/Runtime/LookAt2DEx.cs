7f155bef8bb9152f428853afa6b6e4ba21b3f62e
KYapp <kukiyomenaisapp@gmail.com>
2023-03-05 19:04:50 +0900

    Vector3のLookAt2Dを作成

--- Assets/popoExtentions/Runtime/LookAt2DEx.cs
+++ Assets/popoExtentions/Runtime/LookAt2DEx.cs
@@ -4,5 +4,10 @@ namespace JuhaKurisu.PopoTools.Extentions
 {
     public static class LookAt2DEx
     {
+        public static void LookAt2D(this Transform transform, Vector3 targetPosition)
+        {
+            Vector3 diff = (targetPosition - transform.position);
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, diff);
+        }
     }
 }
